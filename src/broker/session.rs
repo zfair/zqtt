403dfd409742cf8f8f954c7f4bad2db02ffafab7
locustchen <locustchen@tencent.com>
2020-03-08 22:27:40 +0800

    reconstruct StreamHandler::handle method

--- src/broker/session.rs
+++ src/broker/session.rs
@@ -1,6 +1,9 @@
 use actix::prelude::*;
+use actix::fut::IntoActorFuture;
+use actix::fut::FutureWrap;
 use log::{error, info};
 use mqtt3::{self, Packet};
+use std::pin::Pin;
 use std::io;
 use std::net;
 use std::time::Instant;
@@ -39,11 +42,34 @@ impl Session {
             connect_at: None,
         }
     }
-}
 
-#[derive(Message)]
-#[rtype(result = "()")]
-struct MqttConnect(mqtt3::Connect);
+    fn on_connect(&mut self, m: &mqtt3::Connect, ctx: &mut <Self as Actor>::Context) -> impl ActorFuture<Output = Result<mqtt3::Packet, MailboxError>, Actor = Self> {
+        let f = self.broker_addr
+            .send(server::SessionConnect {
+                session_id: self.luid,
+                session_addr: ctx.address(),
+            })
+            .into_actor(self)
+            .then(|res, act, ctx| {
+                match res {
+                    Ok(_) => {
+                        act.connect_at = Some(Instant::now());
+                        fut::ok(Packet::Connack(mqtt3::Connack {
+                            session_present: false,
+                            code: mqtt3::ConnectReturnCode::Accepted,
+                        }))
+                        
+                    }
+                    Err(e) => {
+                        error!("e: {}", e);
+                        ctx.stop();
+                        fut::err(e)
+                    }
+                }
+            });
+            f
+    }
+}
 
 /// To use `Framed` with an actor, we have to implement `StreamHandler` trait
 impl StreamHandler<Result<Packet, io::Error>> for Session {
@@ -51,35 +77,25 @@ impl StreamHandler<Result<Packet, io::Error>> for Session {
     fn handle(&mut self, msg: Result<Packet, io::Error>, ctx: &mut Self::Context) {
         match msg {
             Ok(packet) => {
-                match packet {
+                let res = match packet {
                     // TODO: implement all mqtt packet type
-                    Packet::Connect(_) => {
-                        self.broker_addr
-                            .send(server::SessionConnect {
-                                session_id: self.luid,
-                                session_addr: ctx.address(),
-                            })
-                            .into_actor(self)
-                            .then(|res, act, ctx| {
-                                match res {
-                                    Ok(_) => {
-                                        act.connect_at = Some(Instant::now());
-                                        act.writer.write(Packet::Connack(mqtt3::Connack {
-                                            session_present: false,
-                                            code: mqtt3::ConnectReturnCode::Accepted,
-                                        }));
-                                    }
-                                    Err(e) => {
-                                        error!("e: {}", e);
-                                        ctx.stop()
-                                    }
-                                }
-                                fut::ready(())
-                            })
-                            .wait(ctx);
-                    }
+                    Packet::Connect(m) => {
+                        self.on_connect(&m, ctx)
+                    },
                     _ => unimplemented!(),
-                }
+                };
+                res.then(|res, act, ctx| {
+                    match res {
+                        Ok(m) => {
+                            act.writer.write(m);
+                        },
+                        Err(e) => {
+                            error!("e: {}", e);
+                            ctx.stop()
+                        }
+                    }
+                    fut::ready(())
+                }).wait(ctx)
             }
             Err(e) => {
                 error!("e {}", e);
